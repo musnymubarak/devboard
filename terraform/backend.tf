d1f0f10268b1e61c4e300aa7b821b0050223bc5f
musnymubarak <musnymohammed@gmail.com>
2026-03-10 16:05:21 +0530

    feat: phase 3 complete — kubeadm cluster bootstrapped, 3 nodes ready, WSL kubectl configured, start/stop scripts

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "devboard-terraform-state-322725461163"
-    key            = "devboard/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "devboard-terraform-locks"
-    encrypt        = true
+    bucket       = "devboard-terraform-state-322725461163"
+    key          = "devboard/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
